fbb680961d2c17a58e520906f7338e7f36f197cb
BoppreH <boppreh@gmail.com>
2013-06-06 16:39:53 -0300

    Add symmetric encryption

--- simplecrypto.py
+++ simplecrypto.py
@@ -1,4 +1,12 @@
 import hashlib
+import math
+import base64
+from Crypto.Cipher import DES, AES
+from Crypto import Random
+
+random_instance = Random.new()
+
+algorithms = {'aes': AES, 'des': DES}
 
 def sha1(message):
     return hashlib.sha1(message).hexdigest()
@@ -11,3 +19,41 @@ def sha256(message):
 
 def sha512(message):
     return hashlib.sha152(message).hexdigest()
+
+def str_to_base64(message):
+    return base64.b64encode(message)
+
+def base64_to_str(message):
+    return base64.b64decode(message)
+
+def pad(message, length, padding=' '):
+    return message + (length - len(message)) * padding
+
+def pad_multiple(message, len_multiple, padding=' '):
+    next_length = math.ceil(len(message) / float(len_multiple)) * len_multiple
+    return pad(message, int(next_length), padding)
+
+def random(n_bytes):
+    return random_instance.read(n_bytes)
+
+def encrypt(message, password, algorithm='aes'):
+    cls = algorithms[algorithm]
+    iv = random(cls.block_size)
+    instance = cls.new(pad_multiple(password, 16),
+                       cls.MODE_CFB,
+                       iv)
+    return str_to_base64(iv + instance.encrypt(message))
+
+def decrypt(message, password, algorithm='aes'):
+    message = base64_to_str(message)
+    iv, message = message[:AES.block_size], message[AES.block_size:]
+    instance = AES.new(pad_multiple(password, 16),
+                       AES.MODE_CFB,
+                       iv)
+    return instance.decrypt(message)
+
+def encrypt_aes(message, password):
+    return encrypt(message, password, 'aes')
+
+def decrypt_aes(message, password):
+    return decrypt(message, password, 'aes')
