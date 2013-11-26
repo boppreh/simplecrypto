4c9a1848d0e90dc341874908b305f0fd2daca667
BoppreH <boppreh@gmail.com>
2013-11-26 11:51:19 -0200

    Change setup

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from distutils.core import setup
 
 setup(
-    name='Simplecrypto',
+    name='simplecrypto',
     version=open('CHANGES.txt').read().split()[0],
     author='Lucas Boppre Niehues',
     author_email='lucasboppre@gmail.com',
     packages=['simplecrypto'],
-    url='http://pypi.python.org/pypi/Simplecrypto/',
+    url='http://pypi.python.org/pypi/simplecrypto/',
     license='LICENSE.txt',
     description='Simplecrypto',
     long_description=open('README.md').read(),
+
+    install_requires=[
+        'PyCrypto',
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+    ],
 )
