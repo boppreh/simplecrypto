4a6efa0b7b477bd9d4299335ca1653fb00689d3c
BoppreH <boppreh@gmail.com>
2013-12-05 15:25:24 -0200

    Update 'setup.py'

--- setup.py
+++ setup.py
@@ -5,10 +5,11 @@ setup(
     version=open('CHANGES.txt').read().split()[0],
     author='Lucas Boppre Niehues',
     author_email='lucasboppre@gmail.com',
-    packages=['simplecrypto'],
+    packages=['simplecrypto', 'tests'],
     url='https://github.com/boppreh/simplecrypto',
-    license='LICENSE.txt',
-    description='simplecrypto',
+    license='MIT',
+    description='Simple cryptographic library for hashing and encrypting',
+    keywords = 'simple cryptography symmetric asymmetric hash encrypt decrypt rsa aes sha md5',
     long_description=open('README.rst').read(),
 
     install_requires=[
@@ -19,5 +20,10 @@ setup(
         'Development Status :: 3 - Alpha',
         'Topic :: Security :: Cryptography',
         'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
